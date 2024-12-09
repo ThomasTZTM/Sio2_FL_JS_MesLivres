d078fbe8db608e1f552321c6753c4f5f2e62ae1a
michelint <lemoderateurdelive@gmail.com>
2024-12-09 11:18:30 +0100

    Maj : Gestion du reset du formulaire lorsque celui ci est caché et re-afficher

--- src/ui/gestionnaires.js
+++ src/ui/gestionnaires.js
@@ -5,12 +5,16 @@ export const setupGestionnaire = () => {
     // Récupérer les éléments dans le DOM
     const toggleFormBtn = document.querySelector("#toggleFormBtn")
     const formSection = document.querySelector("#formSection")
-
-    //
     const formCollapse = new bootstrap.Collapse(formSection, {toggle : false})
+    const livreForm = document.querySelector("#bookForm")
 
     // Gestionnaire click bouton toggleFormBtn
     toggleFormBtn.addEventListener("click", ()=> {
         formCollapse.toggle()
+    })
+
+    // On reset le formulaire lorsque celui ci est caché
+    formSection.addEventListener('hidden.bs.collapse', () =>{
+        livreForm.reset()
     })
 }
